3f07163af79ebcf5ac2eb0bf6d3c41be1cdbb550
UserOnline21 <1860362@fife.ac.uk>
2025-11-04 12:05:36 +0000

    updated program with notes.

--- HelloWorldCS/Program.cs
+++ HelloWorldCS/Program.cs
@@ -1,7 +1,9 @@
 ﻿
 // START
 
-// PRINT "Hello, World!"
-Console.WriteLine("Hello, World!");
+// PRINT "Welcome to the underground."
+// the console.writeline acts like the "Print" command in HTML. displaying the text in quotes on the screen.
+// the quotes are needed for the program to understand that it is text.
+Console.WriteLine("Welcome to the underground.");
 
 // END
